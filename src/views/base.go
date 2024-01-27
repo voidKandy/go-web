960d030d92b439b8259705fb5f6e7b2edc73640d
voidKandy <voidkandy@gmail.com>
2024-01-27 10:01:30 +0200

    no more slideshow for album art

--- src/views/base.go
+++ src/views/base.go
@@ -5,12 +5,6 @@ import (
 	"net/http"
 )
 
-//
-// func InitializeViewRoutes() {
-// 	http.Handle("/", http.StripPrefix("/", fs))
-// 	http.HandleFunc("/views/project/", RenderProjects)
-// }
-
 func RenderProject(w http.ResponseWriter, p Project) {
 	tmpl, _ := template.ParseFiles("html/partials/projects.html")
 	tmpl.Execute(w, p)
