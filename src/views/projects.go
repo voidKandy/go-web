5e9e64b40fe2e0285033f1ec16fc1dfe4965c389
voidKandy <voidkandy@gmail.com>
2024-01-27 10:01:40 +0200

    better copy on projects page

--- src/views/projects.go
+++ src/views/projects.go
@@ -18,36 +18,53 @@ type Project struct {
 
 var Projects = []Project{
 	{
-		Path:        "espionox",
-		Name:        "Espionox",
-		Description: "A Rust library for creating Ai driven Agent workflows.",
-		ImagePath:   "public/assets/espionox-gui-screenshot.png",
+		Path: "espionox",
+		Name: "Espionox",
+		Description: `
+           Espionox is an library designed as a Rust alternative to Lang-Chain.
+           Though still in its early stages of development, this library gives developers the ability to construct
+           multi-agent environments. The key feature is the EnvListener trait, which provides an interface for creating dynamic protocols
+           for implementing methods for model self consistancy, or increasing output quality through methods such as Tree-Of-Thought.
+           Currently in active development are retrieval augmented guidance and tool use functionalities.
+           Espionox is published on crates.io and can easily be installed using cargo.
+        `,
+		ImagePath: "",
 		Links: LinkInfo{
 			HasRepo:    true,
-			GitUrl:     "https://github.com/voidKandy/espionox_lib",
-			HasWebDemo: true,
-			DemoUrl:    "web-demo",
+			GitUrl:     "https://github.com/voidKandy/Espionox",
+			HasWebDemo: false,
+			DemoUrl:    "",
 		},
 		Tags: []string{"Rustlang", "Applied Ai", "System Design"},
 	},
 	{
-		Path:        "espionox-demo",
-		Name:        "Espionox Web Demo",
-		Description: "A chat web app utilizing Espionox",
-		ImagePath:   "public/assets/espionox-gui-screenshot.png",
+		Path: "bureau",
+		Name: "Bureau",
+		Description: `
+            Bureau is essentially a chatGpt clone app that utilizes Espionox under the hood. It is a simple example of how to use
+            Espionox to create an LLM application. I used HTMX with Rust's Askama templating language, there is no live webapp yet, but 
+            Bureau is easy to clone and get up and running on localhost.
+        `,
+		ImagePath: "",
 		Links: LinkInfo{
-			HasRepo:    false,
-			GitUrl:     "",
+			HasRepo:    true,
+			GitUrl:     "https://github.com/voidKandy/bureau",
 			HasWebDemo: false,
 			DemoUrl:    "",
 		},
-		Tags: []string{"Axum", "Htmx"},
+		Tags: []string{"Axum", "Htmx", "Espionox"},
 	},
 	{
-		Path:        "showbeam",
-		Name:        "Showbeam",
-		Description: "A web app for sharing and creating markdown slide shows",
-		ImagePath:   "public/assets/espionox-gui-screenshot.png",
+		Path: "showbeam",
+		Name: "Showbeam",
+		Description: `
+            Showbeam is a web application for creating and sharing markdown slideshows. It works much like google slides,
+            providing users with an easy interface for creating markdown document slideshows. I built this with a friend of mine as an
+            excersize. The backend was written using the Rust library, Actix-Web, and we used planetscale for our database. The frontend was 
+            written using SvelteKit.
+            
+            `,
+		ImagePath: "",
 		Links: LinkInfo{
 			HasRepo:    false,
 			GitUrl:     "",
